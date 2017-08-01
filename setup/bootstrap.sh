bba64ac1df2c5db4c3a20084900b15f2fd761e9b
shahab1363 <shahab1363@yahoo.com>
2017-08-01 17:05:53 -0400

    Update bootstrap.sh

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -28,7 +28,7 @@ if [ ! -d $HOME/mailinabox ]; then
 	echo Downloading Mail-in-a-Box $TAG. . .
 	git clone \
 		-b $TAG --depth 1 \
-		https://github.com/mail-in-a-box/mailinabox \
+		https://github.com/shahab1363/mailinabox \
 		$HOME/mailinabox \
 		< /dev/null 2> /dev/null
 
