09a0b21bfa7ff6c3cba0ea6f7ad54fe85a7f9d4f
shahab1363 <shahab1363@yahoo.com>
2017-08-01 17:50:37 -0400

    Update status_checks.py

--- management/status_checks.py
+++ management/status_checks.py
@@ -798,7 +798,7 @@ def get_latest_miab_version():
     from socket import timeout
 
     try:
-        return re.search(b'TAG=(.*)', urlopen("https://mailinabox.email/setup.sh?ping=1", timeout=5).read()).group(1).decode("utf8")
+        return re.search(b'TAG=(.*)', urlopen("Xhttps://mailinabox.email/setup.sh?ping=1", timeout=5).read()).group(1).decode("utf8")
     except (HTTPError, URLError, timeout):
         return None
 
